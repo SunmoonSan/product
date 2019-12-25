a1aa578511c25456b6d899ab73b8684b62820379
SunmoonSan <18814184179@163.com>
2019-12-25 23:44:18 +0800

    user模块

--- datamodels/user.go
+++ datamodels/user.go
@@ -1,1 +1,9 @@
 package datamodels
+
+type User struct {
+	ID int64 `json:"id" form:"ID" sql:"ID`
+	Nickname string `json:"nickName" form:"nickName" sql:"nickName"`
+	UserName string `json:"userName" form:"userName" sql:"userName"`
+	HashPassword string `json:"-" form:"passWord" sql:"passWord"`
+
+}
--- repositories/user_repository.go
+++ repositories/user_repository.go
@@ -1,1 +1,66 @@
 package repositories
+
+import (
+	"database/sql"
+	"errors"
+	"product/common"
+	"product/datamodels"
+)
+
+type IUserRepository interface {
+	Conn() error
+	Select(userName string) (user *datamodels.User, err error)
+	Insert(user *datamodels.User) (userId int64, err error)
+}
+
+func NewUserManagerRepository(table string, db *sql.DB) IUserRepository {
+	return &UserManagerRepository{
+		table:     table,
+		mysqlConn: db,
+	}
+}
+
+type UserManagerRepository struct {
+	table     string
+	mysqlConn *sql.DB
+}
+
+func (u *UserManagerRepository) Conn() (err error) {
+	if u.mysqlConn == nil {
+		mysql, errMysql := common.NewMysqlConn()
+		if errMysql != nil {
+			return errMysql
+		}
+		u.mysqlConn = mysql
+	}
+	if u.table == "" {
+		u.table = "user"
+	}
+	return
+}
+
+func (u *UserManagerRepository) Select(userName string) (user *datamodels.User, err error) {
+	if userName == "" {
+		return &datamodels.User{}, errors.New("条件不能为空! ")
+	}
+	if err = u.Conn(); err != nil {
+		return &datamodels.User{}, err
+	}
+	sql := "Select * from" + u.table + " where userName=?"
+	rows, errRows := u.mysqlConn.Query(sql, userName)
+	defer rows.Close()
+	if errRows != nil {
+		return &datamodels.User{}, errRows
+	}
+	result := common.GetResultRow(rows)
+	if len(result) == 0 {
+		return &datamodels.User{}, errors.New("用户不存在")
+	}
+	user = &datamodels.User{}
+	common.DataToStructByTagSql(result, user)
+	return
+}
+
+func (u *UserManagerRepository) Insert(user *datamodels.User) (userId int64, err error) {
+	panic("implement me")
+}

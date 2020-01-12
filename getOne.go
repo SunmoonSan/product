03f772dc541095630907f9ba2116a73e1ea0672a
SunmoonSan <18814184179@163.com>
2020-01-12 23:53:12 +0800

    数量控制接口

---
+++ getOne.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"sync"
+)
+
+var sum int64 = 0
+
+// 预存商品数量
+var productNum int64 = 1000
+
+// 互斥锁
+var mutex sync.Mutex
+
+// 获取秒杀商品
+func GetOneProduct() bool {
+	// 加锁
+	mutex.Lock()
+	defer mutex.Unlock()
+	// 判断数据是否超限
+	if sum < productNum {
+		sum += 1
+		return true
+	}
+	return false
+}
+
+func GetProduct()  {
+
+}
+
+func main() {
+	http.HandleFunc("getOne", GetProduct)
+	err :=
+}
